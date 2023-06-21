c895502bc5d21f17ed4e4b41841fc3863360faa6
Vitallios <61820576+vitallios@users.noreply.github.com>
2023-06-21 12:34:50 +0300

    Add files via upload

---
+++ reloude.js
@@ -0,0 +1,30 @@
+
+function page() {
+
+ let timer; // пока пустая переменная
+let x = 5; // стартовое значение обратного отсчета
+countdown(); // вызов функции
+function countdown() {
+  // функция обратного отсчета
+  // let nums = 
+  document.getElementById("rocket").innerHTML = x;
+  x--; // уменьшаем число на единицу
+  if (x < 0) {
+    clearTimeout(timer); // таймер остановится на нуле
+    stepHomePage(); // вызываем функцию перехода на стартовую страницу
+  } else {
+    timer = setTimeout(countdown, 1000);
+  }
+}
+
+
+
+
+
+
+ setTimeout(()=>{
+   window.location.pathname = '/index.html';
+ }, 5000)
+}
+
+page()
